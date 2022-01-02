cff13052de8890c31a3650760daaa3ab695ebfd0
lucaincarnato <lucaincarnato.03@gmail.com>
2022-01-02 15:15:23 +0100

    GUI design and text showing

--- src/main.py
+++ src/main.py
@@ -12,28 +12,43 @@ class App():
         icon = tk.PhotoImage(file="/home/lucaincarnato/Scrivania/pycalculator/src/icon.png")
         window.iconphoto(False, icon)
 
+        #This frame will contain all the button, so it is possible to make a larger entry
+        frame = tk.Frame(window, background="#F8F1FF")
+        frame.grid(column=0, row=0, pady=10, padx=0)
+
         #Configuring an array of buttons to create them easily
         #The text_buttons array is defined in layers so it is easier to assign a text to its specific button
-        text_buttons = [["1", "2", "3", "+"], ["4", "5", "6", "-"], ["7", "8", "9", ":"], ["<-", "0", "=", "*"]]
-        buttons = []
+        text_buttons = [["1", "2", "3", "+"], ["4", "5", "6", "-"], ["7", "8", "9", ":"], ["(", "0", ")", "*"]]
+        buttons = [[], [], [], []]
+
+        #Rendering all the buttons, witch texts are defined below
         for i in range(4):
             for j in range(4):
-                button = tk.Button(window, text=text_buttons[i][j], width=10)
+                #The lambda function creates for every i and j an istance that refers to each button, so that onclick the command attribute goes to pick the right text
+                button = tk.Button(frame, text=text_buttons[i][j], width=10, command=lambda c=i, d=j: entry.insert(tk.END, text_buttons[c][d]))
                 button.grid(row=i, column=j, pady=10, padx=10)
-                buttons.append(button)
-                #? Add the script to insert in the entry
+                buttons[i].append(button)
+                #! It returns only the last char
 
         #Entry widget to write buttons' content
-        entry = tk.Text(width=13, height=5)
-        entry.grid(row=5, padx=10, pady=10)
+        entry = tk.Text(window, width=60, height=5)
+        entry.grid(column=0, row=5, padx=10, pady=10)
+
+        #Button to cancel the last character
+        deny = tk.Button(frame, text="<-")
+        deny.grid(row=5, column=0, padx=10, pady=10, sticky="WEN")
 
         #Button that makes the calculus be saved in a txt file
-        save = tk.Button(window, text="Save")
+        save = tk.Button(frame, text="Save")
         save.grid(row=5, column=1, padx=10, pady=10, sticky="WEN")
 
         #Button that delete the operation
-        save = tk.Button(window, text="Cancel")
-        save.grid(row=5, column=2, padx=10, pady=10, sticky="WEN")
+        cancel = tk.Button(frame, text="Cancel")
+        cancel.grid(row=5, column=2, padx=10, pady=10, sticky="WEN")
+
+        #Button to cancel the last character
+        solve = tk.Button(frame, text="=")
+        solve.grid(row=5, column=3, padx=10, pady=10, sticky="WEN")
 
     def start(self):
         if __name__ != "__main__": return
